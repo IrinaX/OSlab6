2bb0089326b638213a4f82ed0716d6377d257519
IrinaX <iksanova08@mail.ru>
2021-02-15 00:15:37 +0500

    done

---
+++ Client/Client.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace Client
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            const string IP = "127.0.0.1";
+            const int PORT = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            tcpSocket.Connect(tcpEndPoint);//подключаемся к серверу
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Type a message: ");
+                    var message = Console.ReadLine();
+
+                    var data = Encoding.UTF8.GetBytes(message);//получаем введенные данные
+
+                    tcpSocket.Send(data);//отправляем данные
+                    var buffer = new byte[256];//буфер на 256 байт
+                    var size = 0; //количество реально полученных байтов
+                    var answer = new StringBuilder(); //здесь будем сохранять ответ с сервера
+
+                    do
+                    {
+                        size = tcpSocket.Receive(buffer);
+
+                        answer.Append(Encoding.UTF8.GetString(buffer, 0, size));//раскодировка полученных байтов
+
+
+                    } while (tcpSocket.Available > 0);//пока есть данные, обрабатываем их
+
+                    Console.WriteLine("Answer: " + answer);
+
+                }
+                catch 
+                {
+
+                    Console.WriteLine("Connection error! Enter any key to continue...");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            tcpSocket.Shutdown(SocketShutdown.Both);//закрываем сокет на клиенте и сервере
+            tcpSocket.Close();
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            const string IP = "127.0.0.1";
+            const int PORT = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(IP), PORT);
+
+            var tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            tcpSocket.Bind(tcpEndPoint);
+            tcpSocket.Listen(5);//5 endPoints
+            var listener = tcpSocket.Accept();//сокет для нового клиента
+            while (true)
+            {
+                try
+                {
+                    var buffer = new byte[256];//буфер на 256 байт
+                    var size = 0; //количество реально полученных байтов
+                    var data = new StringBuilder(); //здесь будем сохранять полученные данные
+
+                    do
+                    {
+                        size = listener.Receive(buffer);
+                        data.Append(Encoding.UTF8.GetString(buffer, 0, size));//раскодировка полученных байтов
+                    } while (listener.Available > 0);//пока есть данные, обрабатываем их
+
+                    Console.WriteLine("Answer: " + data);
+
+                    Console.WriteLine("Type a message: ");
+                    var message = Console.ReadLine();
+                    var newData = Encoding.UTF8.GetBytes(message);//получаем введенные данные
+                    listener.Send(newData);//отправляем данные
+                }
+                catch
+                {
+
+                    Console.WriteLine("Connection error! Enter any key to continue...");
+                    Console.ReadLine();
+                    return;
+                }
+            }
+            listener.Shutdown(SocketShutdown.Both);//закрываем сокет на клиенте и сервере
+            listener.Close();
+        }
+    }
+}
